6a337b49b866c7eea5fbecfeaa107c162d635e87
Vignesh-TJ <vigneshtj07@gmail.com>
2025-02-24 20:24:48 +0530

    co

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,35 +1,13 @@
-import React from 'react'
-import './Auth.css'
-import { Link } from 'react-router-dom'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from '../App.jsx'
+import "../src/bootstrap.min.css"
+import { BrowserRouter } from 'react-router-dom'
 
-function Main() {
-  return (
-<>
-
-<div className="container-fluid main">
-
-    <div className="row">
-      <div className="col-md-4"></div>
-      <div className="col-md-4">
-
-        <div className='d-flex align-items-center justify-content-center' style={{height:'100vh'}} >
-
-           <div> 
-         
-         <Link to={'/login'}>
-         <div className='text-center'><button className='btn btn-warning text-black px-4 my-3' style={{fontSize:'20px'}}>Farmer Login</button></div></Link>
-         </div>
-
-        </div>
-
-      </div>
-      <div className="col-md-4"></div>
-    </div>
-
-</div>
-
-</>
-  )
-}
-
-export default Main
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+    <App /></BrowserRouter>
+  </StrictMode>,
+)
